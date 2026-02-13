c6bc931a60320c2dd72fdcfb20bec4e92e0aba3e
yashsalvi98 <yashsalvi75@gmail.com>
2026-02-13 15:47:04 +0530

    Fixed CORS again

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://innodecor-frontend02.vercel.app/',
+  origin: "*",
   credentials: true
 }));
 app.use(bodyParser.json());
